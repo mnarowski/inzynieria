0778417715762fbfd2eb03f44a74af325f0f1724
Marcin S. Narowski <axxxon.pl@gmail.com>
2012-11-26 19:21:54 +0100

    Migracje dodane do projektu

---
+++ Migrations/0001.sql
@@ -0,0 +1,13 @@
+﻿-- Database: inzynieria
+
+-- DROP DATABASE inzynieria;
+
+CREATE DATABASE inzynieria
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+GRANT CONNECT, TEMPORARY ON DATABASE inzynieria TO public;
+GRANT ALL ON DATABASE inzynieria TO postgres;
