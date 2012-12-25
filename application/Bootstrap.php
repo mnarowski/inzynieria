f41f77e95dfa902890d6e80061638030818a475d
Marcin S. Narowski <axxxon.pl@gmail.com>
2012-12-25 15:09:56 +0100

    few changes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,22 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+    
+    public function _initLayout(){
+        
+    }
+    
+    public function _initDatabase(){
+        
+    }
+    
+    public function _initCache(){
+        
+    }
+    
+    public function _initNavigation(){
+        
+    }
 
 }
 
