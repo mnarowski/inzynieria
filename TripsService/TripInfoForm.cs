2134213bb226185d86b8315ca13307caba6e3609
krz128 <krz128@gmail.com>
2012-12-30 12:53:34 +0100

    #0000053

---
+++ TripsService/TripInfoForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TripsService
+{
+    public partial class TripInfoForm : Form
+    {
+        private object PhotoGalleryLibrary;
+        public TripInfoForm()
+        {
+
+        }
+
+        private void propertyGrid1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TripInfoForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
