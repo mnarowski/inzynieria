c5286eb7d0465ba3255bb2156570885607e5299b
pawelbombik <pawel.bombik@o2.pl>
2013-01-22 18:26:53 +0100

    Poprawki

--- TripsService/SoundManager.cs
+++ TripsService/SoundManager.cs
@@ -1,24 +1,38 @@
 using System;
 using TripsService;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TripsService {
 	public class SoundManager {
 
-        private SoundPlayer mSoundPlayer = null;
+        private string sPathToPlaylist = string.Empty;
 
-		private SoundManager() {
-			throw new System.Exception("Not implemented");
+		public SoundManager(string pathToPlayList) {
+            this.sPathToPlaylist = pathToPlayList;
 		}
-		public bool Play(String resource) {
-            if (mSoundPlayer == null) {
-                mSoundPlayer = new SoundPlayer();
+
+        public List<MP3_File> GetPlaylist()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(this.sPathToPlaylist);
+            List<MP3_File> files = new List<MP3_File>();
+
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                files.Add(new MP3_File(file));
             }
 
+            return files;
+        }
+        public bool IsEnabled()
+        {
             return true;
         }
 		public void Stop() {
-			throw new System.Exception("Not implemented");
+			//Obs³uga przez Windows Media Player
 		}
+        
         ///
 		public bool IsEnabled() {
             return true;
@@ -33,4 +47,20 @@ namespace TripsService {
 
     }
 
+}
+public class MP3_File
+{
+    FileInfo info = null;
+
+    public string PathToMP3 { get { return this.info.FullName.ToString(); } }
+
+    public MP3_File(FileInfo fileMp3)
+    {
+        this.info = fileMp3;
+    }
+
+    public override string ToString()
+    {
+        return this.info.Name.ToString();
+    }
 }
