d450729f7ce9790017641369a2a38ad2f62321bc
Marcin S. Narowski <axxxon.pl@gmail.com>
2013-01-26 23:42:42 +0100

    Kolejne formatki 2

---
+++ TripsService/FindAttraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TripsService
+{
+    public partial class FindAttraction : Form
+    {
+        public FindAttraction()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
